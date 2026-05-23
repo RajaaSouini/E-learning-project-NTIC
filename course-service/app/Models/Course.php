aef9e8c5246b7b3e9018e598d04a1607135d4282
RajaaSouini <rajaasouini@gmail.com>
2026-05-23 23:04:55 +0100

    dashboard

--- course-service/app/Models/Course.php
+++ course-service/app/Models/Course.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $fillable = [
-        'title',
-        'description',
-        'slug',
-        'technology',
-        'level',
-        'duration',
-        'video_url',
-        'thumbnail',
-        'professor_id',
-        'status',
-    ];
+    'title',
+    'description',
+    'slug',
+    'technology',
+    'level',
+    'duration',
+    'video_url',
+    'thumbnail',
+    'professor_id',
+    'status',      
+];
 
     // Un cours appartient à un chapitre
     public function chapter()
